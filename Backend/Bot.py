565d5281a7138b4ac22e9d3e858b1c2fcc7d1d0f
mizerhit <mizerhit@gmail.com>
2023-11-29 16:34:32 +0800

    Add tg bot in Bot.py and fix some problems in DataBase.py

---
+++ Backend/Bot.py
@@ -0,0 +1,70 @@
+import telebot
+from telebot import types
+import DataBase
+    
+db = DataBase.DataAccessObject()
+
+token = "6906791170:AAFigjd2Hjh9x_vXzgEg3pf5MCiBGrg1Nw4"
+bot = telebot.TeleBot(token)
+
+# menu_mark = types.ReplyKeyboardMarkup(
+#     one_time_keyboard=False, resize_keyboard=True, row_width=2)
+# homework_mark = types.ReplyKeyboardMarkup(
+#     one_time_keyboard=True, resize_keyboard=True, row_width=2)
+# subject_mark = types.ReplyKeyboardMarkup(
+#     one_time_keyboard=True, resize_keyboard=True, row_width=2)
+
+# menu_btn1 = types.KeyboardButton("Расписание")
+# menu_btn3 = types.KeyboardButton("Просмотр дз")
+# menu_btn4 = types.KeyboardButton("Добавление дз")
+# menu_btn5 = types.KeyboardButton("Изменение расписания")
+# menu_mark.add(menu_btn1, menu_btn3, menu_btn4, menu_btn5)
+
+# homework_btn2 = types.KeyboardButton("Добавление дз")
+# homework_btn1 = types.KeyboardButton("Просмотр дз")
+# homework_mark.add(homework_btn1, homework_btn2)
+
+# subject_btn2 = types.KeyboardButton("Выберите предмет")
+# subject_mark.add(subject_btn2)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    db.DataUpdate()
+    tmp = db.get_things()
+    bot.send_message(message.from_user.id, str(tmp))
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    nickname = message.chat.username
+    if message.text == "Обновление данных":
+        db.DataUpdate()
+        bot.send_message(message.from_user.id, 'Успешно')
+
+    elif message.text == "Типы товаров":
+        names = {}
+        for name in db.get_things():
+            names[name] = 1
+        for key in names.keys():
+            bot.send_message(message.from_user.id, key)
+
+    elif message.text == "Выбрать товар":
+        bot.send_message(message.from_user.id, "Введите название товара из типов товаров")
+        bot.register_next_step_handler(message, GetType)
+
+    # else:
+    #     main.PrintDiscount()
+
+def GetType(message):
+    type = message.text
+    print(db.get_thing(type))
+    for name in db.get_thing(type):
+        bot.send_message(message.from_user.id, str(name))
+        print(name)
+    # bot.send_message(message.from_user.id, db.get_thing(type))
+    # bot.send_message(message.from_user.id, "Домашняя работа добавлена",
+    #                  reply_markup=menu_mark)
+
+
+bot.polling(none_stop=True, interval=0)
---
+++ Backend/DataBase.py
@@ -0,0 +1,66 @@
+import sqlite3
+import requests
+from bs4 import BeautifulSoup
+import json
+
+class DataAccessObject:
+    __instance = None
+
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+            cls.__instance.__instance = sqlite3.connect('db.sqlite3', check_same_thread=False)
+            cls.__instance.__cursor = cls.__instance.__instance.cursor()
+            cls.__instance.__create_tables()
+        return cls.__instance
+
+    def __del__(self):
+        self.__instance.close()
+        self.__instance = None
+
+    def __create_tables(self):
+        self.__cursor.execute('''
+            CREATE TABLE IF NOT EXISTS Things (
+                thing TEXT NOT NULL,
+                color TEXT NOT NULL,          
+                price INTEGER,
+                discount INTEGER
+            )
+        ''')
+        self.__instance.commit()
+
+    def save_thing(self, name, colour, price, discount):
+        self.__cursor.execute('INSERT OR REPLACE INTO Things (thing, color, price, discount) VALUES (?, ?, ?, ?)',
+                              (name, colour, price, discount))
+        self.__instance.commit()
+
+    def get_things(self):
+        self.__cursor.execute('SELECT thing FROM Things')
+        return self.__cursor.fetchall()
+
+    def get_thing(self, name):
+        self.__cursor.execute('SELECT * FROM Things WHERE thing = ?', (name,))
+        return self.__cursor.fetchall()
+
+    def get_discount(self):
+        self.__cursor.execute('SELECT * FROM Things ORDER BY discount DESC')
+        return self.__cursor.fetchall()
+    
+    def DataUpdate(self):
+        url = "https://www.colins.ru/c/muzh-2?specs=7"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+
+            soup = BeautifulSoup(response.text, 'lxml')
+
+            all_data = soup.findAll('div', class_='productCartMain')
+
+            for data in all_data:
+                div = data.div
+                thing_data = json.loads(div.attrs["data-ga"])
+                name = thing_data.get("name", "")
+                discount = thing_data.get("discount", "")
+                price = thing_data.get("price", "")
+                colour = thing_data.get("variant", "")
+                self.save_thing(name, colour, price, discount)
