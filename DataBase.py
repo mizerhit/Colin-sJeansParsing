6303daf72e6021f5c6273ccd3009d4022b76feaf
mizerhit <mizerhit@gmail.com>
2023-11-29 13:49:22 +0800

    Change parsing code in main.py and add database in DataBase.py

---
+++ DataBase.py
@@ -0,0 +1,47 @@
+import sqlite3
+
+class DataAccessObject:
+    __instance = None
+
+    def __init__(self):
+        self.__instance = sqlite3.connect('db.sqlite3')
+        self.__cursor = self.__instance.cursor()
+        self.__create_tables()
+    
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
+    
+    def __del__(self):
+        self.__instance.close()
+        self.__instance = None
+
+    def __create_tables(self):
+        self.__cursor.execute("DROP TABLE IF EXISTS Things")
+        self.__cursor.execute('''
+            CREATE TABLE Things (
+            thing TEXT NOT NULL,
+            color TEXT NOT NULL,          
+            price INTEGER,
+            discount INTEGER
+            )
+            ''')
+        self.__instance.commit()
+
+    def save_thing(self, name, colour, price, discount):
+        self.__cursor.execute('INSERT INTO Things (thing, color, price, discount) VALUES (?, ?, ?, ?)', (name, colour, price, discount))
+    
+    def get_things(self):
+        self.__cursor.execute('''
+            SELECT thing FROM Things
+        ''')
+        return self.__cursor.fetchall()
+    
+    def get_thing(self, name):
+        self.__cursor.execute('SELECT * FROM Things WHERE thing = ?', (name,))
+        return self.__cursor.fetchall()
+    
+    def get_discount(self):
+        self.__cursor.execute('SELECT * FROM Things ORDER BY discount DESC')
+        return self.__cursor.fetchall()
--- main.py
+++ main.py
@@ -1,38 +1,44 @@
 import requests
 from bs4 import BeautifulSoup
+import DataBase as db
+import json
+
+database = db.DataAccessObject()
 
 url = "https://www.colins.ru/c/muzh-2?specs=7"
 
 response = requests.get(url)
+def UpdateData():
+    if response.status_code == 200:
 
-if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'lxml')
+        title = soup.title.text
 
-    soup = BeautifulSoup(response.text, 'lxml')
-    title = soup.title.text
+        all_data = soup.findAll('div', class_='productCartMain')
 
-    all_data = soup.findAll('div', class_='productCartMain')
+        for data in all_data:
+            div = data.div
+            thing_data = json.loads(div.attrs["data-ga"])
+            name = thing_data.get("name", "")
+            discount = thing_data.get("discount", "")
+            price = thing_data.get("price", "")
+            colour = thing_data.get("variant", "")
+            database.save_thing(name, colour, price, discount)
+    else:
+        print(f"Faile, status code: {response.status_code}")
 
-    for data in all_data:
-        div = data.div
-        thing = div.attrs["data-ga"]
-        str(thing)
 
-        name_pos = thing.find('"name"')
-        discount_pos = thing.find('"discount"')
-        price_pos = thing.find('"price"')
-        category_pos = thing.find('"category"')
-        variant_pos = thing.find('"variant"')
-        quantity_pos = thing.find('"quantity"')
+UpdateData()
 
-        name = thing[(name_pos + 7):(discount_pos - 1)]
-        discount = thing[(discount_pos + 11):(price_pos - 1)]
-        price = thing[(price_pos + 8):(category_pos - 1)]
-        variant = thing[(variant_pos + 10):(quantity_pos - 1)]
+names = {}
+thing_names = []
 
-        print("________________________________________________________________________________________________________________________________")
-        print(name)
-        print(discount)
-        print(price)
-        print(variant)
-else:
-    print(f"Faile, status code: {response.status_code}")
+for thing in database.get_things():
+    names[thing] = 1
+
+for name in names.keys():
+    thing_names.append(*name)
+
+# print(thing_names)
+# print(database.get_thing(thing_names[1]))
+print(database.get_discount())
