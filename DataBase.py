74263e735a2f06ad71cf60af6d9a90411e0de967
mizerhit <mizerhit@gmail.com>
2023-12-01 08:45:25 +0800

    Change Database.py with git bug

--- DataBase.py
+++ DataBase.py
@@ -12,26 +12,27 @@ class DataAccessObject:
         if cls.__instance is None:
             cls.__instance = super().__new__(cls)
         return cls.__instance
-    
+
     def __del__(self):
         self.__instance.close()
         self.__instance = None
 
     def __create_tables(self):
-        self.__cursor.execute("DROP TABLE IF EXISTS Things")
         self.__cursor.execute('''
-            CREATE TABLE Things (
-            thing TEXT NOT NULL,
-            color TEXT NOT NULL,          
-            price INTEGER,
-            discount INTEGER
+            CREATE TABLE IF NOT EXISTS Things (
+                thing TEXT NOT NULL,
+                color TEXT NOT NULL,          
+                price INTEGER,
+                discount INTEGER
             )
-            ''')
+        ''')
         self.__instance.commit()
 
     def save_thing(self, name, colour, price, discount):
-        self.__cursor.execute('INSERT INTO Things (thing, color, price, discount) VALUES (?, ?, ?, ?)', (name, colour, price, discount))
-    
+        self.__cursor.execute('INSERT OR REPLACE INTO Things (thing, color, price, discount) VALUES (?, ?, ?, ?)',
+                              (name, colour, price, discount))
+        self.__instance.commit()
+
     def get_things(self):
         self.__cursor.execute('''
             SELECT thing FROM Things
